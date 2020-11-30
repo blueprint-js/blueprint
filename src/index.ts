dfefd88333865e7af7e818d4a354282f954e2618
xpyxel <xpyxel@gmail.com>
2020-11-30 10:05:48 -0500

    Add and setup module-alias

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+// eslint-disable-next-line node/no-unpublished-require
+require('module-alias/register');
 export {Blueprint} from './lib/class/client';
 export {Permissions} from './lib/util/permissions';
