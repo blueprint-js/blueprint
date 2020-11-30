71121f15459d4d28f620ea691abbc9120dad5cb0
xpyxel <xpyxel@gmail.com>
2020-11-30 04:58:53 -0500

    Create utility to load YAML configuration files

---
+++ src/lib/config.ts
@@ -0,0 +1,19 @@
+import {parse} from 'yaml';
+import {readFileSync} from 'fs';
+import {ClientOptions} from 'eris';
+
+/**
+ * The type interface for the bot configuration
+ */
+export interface Config {
+  bot: {token: string; options?: ClientOptions};
+}
+
+/**
+ * Loads a YAML configuration file
+ * @param path The path to a YAML configuration file
+ */
+export function loadConfig(path: string): Config {
+  const data = readFileSync(path, {encoding: 'utf-8'});
+  return parse(data);
+}
