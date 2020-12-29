6b14fc57679f642eb64e136a1d4fa0d4a190c09f
xpyxel <xpyxel@gmail.com>
2020-12-29 12:07:22 -0500

    Update once more

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import 'reflect-metadata';
 export {Blueprint} from './lib/class/client';
 export {Permissions} from './lib/util/permissions';
 export {Command, Executor} from './lib/class/command';
-export {SlashCommand, CommandContext} from 'slash-create';
+export {SlashCommand, SlashCreator, CommandContext} from 'slash-create';
 export {Plugin} from './lib/class/plugin';
 export * as TypeORM from 'typeorm';
 export {Message} from 'eris';
