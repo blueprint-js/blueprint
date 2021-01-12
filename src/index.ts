5b2e06bb08ca3414b5340d1d9ccd1fa4ab09d12e
xpyxel <xpyxel@gmail.com>
2021-01-12 18:46:36 -0500

    Export the client events type

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 export {Blueprint} from './lib/class/client';
 export {Permissions} from './lib/util/permissions';
 export {Command, Executor} from './lib/class/command';
+export {ClientEvents} from './lib/util/types';
 export {Plugin} from './lib/class/plugin';
 export * as TypeORM from 'typeorm';
 export {Message} from 'eris';
