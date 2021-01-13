4877667a4ef632353da4fea9c0c387c0e6fb08e9
xpyxel <xpyxel@gmail.com>
2021-01-13 01:27:34 -0500

    Create the autoregistry class

--- src/lib/class/registry.ts
+++ src/lib/class/registry.ts
@@ -6,3 +6,12 @@ export abstract class Registry<T> {
   abstract register(key: string, value: T): void;
   abstract unregister(key: string): void;
 }
+
+export abstract class AutoRegistry<T> {
+  protected items: Map<string, T>;
+  constructor() {
+    this.items = new Map();
+  }
+  abstract register(value: T): void;
+  abstract unregister(key: string): void;
+}
