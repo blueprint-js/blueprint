47f59d74c973a241cb846b7e473210f6dec9ebda
xpyxel <xpyxel@gmail.com>
2021-01-13 13:23:03 -0500

    Remove broken plugin export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,5 @@ import 'reflect-metadata';
 export {Blueprint} from './lib/class/client';
 export {Command, Executor} from './lib/class/command';
 export {ClientEvents} from './lib/util/types';
-export {Plugin} from './lib/class/plugin';
 export * as TypeORM from 'typeorm';
 export {Message} from 'eris';
