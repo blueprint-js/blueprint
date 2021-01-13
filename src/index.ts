03d7f255e43f4210787a925f3e6ac68fed287974
xpyxel <xpyxel@gmail.com>
2021-01-13 14:32:49 -0500

    Update version and add exports

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import 'reflect-metadata';
 export {Blueprint} from './lib/class/client';
 export {Command, Executor} from './lib/class/command';
 export {ClientEvents} from './lib/util/types';
+export {Message, User, Member} from 'eris';
 export * as TypeORM from 'typeorm';
-export {Message} from 'eris';
