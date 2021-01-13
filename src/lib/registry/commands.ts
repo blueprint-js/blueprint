4b80b782fc93245f540a59caf139ef306904abff
xpyxel <xpyxel@gmail.com>
2021-01-13 14:40:19 -0500

    Make command execution not leak memory

--- src/lib/registry/commands.ts
+++ src/lib/registry/commands.ts
@@ -3,16 +3,16 @@ import {CommandMeta, Executor} from '../class/command';
 import {Member, Message, User} from 'eris';
 import {Blueprint} from '../class/client';
 
-type Class = {new (...args: Array<unknown>): unknown};
+type Command = {new (...args: Array<unknown>): unknown} & Executor;
 
-export class CommandRegistry extends AutoRegistry<Class> {
+export class CommandRegistry extends AutoRegistry<Command> {
   /**
    * Registers a new command
    * @param value The command class to register
    */
-  register(value: Class): void {
+  register(value: Command): void {
     const meta = Reflect.getMetadata('meta', value.prototype) as CommandMeta;
-    this.items.set(meta.name, value);
+    this.items.set(meta.name, new value() as Command);
   }
 
   /**
@@ -36,7 +36,7 @@ export class CommandRegistry extends AutoRegistry<Class> {
       if (meta.aliases.includes(cmd) || key === cmd) {
         const ugroups = ref.groups.check(user);
         if (ugroups.some((g: string) => meta.groups.includes(g)))
-          (new value() as Executor).callback(msg, ref);
+          value.callback(msg, ref);
         break;
       }
     }
