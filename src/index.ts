133c3d06739d83e7a1e7f007ea7edf11e6a26c84
xpyxel <xpyxel@gmail.com>
2021-01-21 17:29:02 -0500

    Allows users to extend the config

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export {Blueprint, Extension} from './lib/class/client';
 export {Command, Executor} from './lib/class/command';
 export {Message, User, Member} from 'eris';
 export {ClientEvents} from './lib/util/types';
+export {BaseConfig} from './lib/util/config';
 export * as TypeORM from 'typeorm';
