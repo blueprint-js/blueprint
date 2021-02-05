56652197ff4f2457a17c776bd82c1e725cc82771
xpyxel <xpyxel@gmail.com>
2021-02-05 00:50:55 -0500

    Begin implementing guards

--- src/lib/class/command.ts
+++ src/lib/class/command.ts
@@ -2,21 +2,30 @@ import {Message} from 'eris';
 import {Blueprint} from '../class/client';
 import {BaseConfig} from '../util/config';
 
-export interface CommandMeta {
+export interface CommandMeta<T extends BaseConfig> {
   name: string;
   aliases: Array<string>;
   groups: Array<string>;
+  guards?: Array<Guard<T>>;
 }
+
+interface PartialMeta<T extends BaseConfig> {
+  aliases: Array<string>;
+  groups: Array<string>;
+  guards?: Array<Guard<T>>;
+}
+
+export type Guard<T extends BaseConfig> = (
+  ctx: Message,
+  ref: Blueprint<T>
+) => boolean;
 
 /**
  * Interface used to enforce the callback signature of a command
  */
 export abstract class Command<T extends BaseConfig> {
-  public readonly meta: CommandMeta;
-  constructor(
-    name: string,
-    meta: {aliases: Array<string>; groups: Array<string>}
-  ) {
+  public readonly meta: CommandMeta<T>;
+  constructor(name: string, meta: PartialMeta<T>) {
     this.meta = {name, ...meta};
   }
   abstract callback(ctx: Message, args: Array<string>, ref: Blueprint<T>): void;
