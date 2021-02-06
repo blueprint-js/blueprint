cda8093aeafd34e7a47044eb923a83a89fc0b316
Hanna <xpyxel@gmail.com>
2021-02-06 18:32:21 +0000

    Add plugin registry

---
+++ src/lib/registry/plugins.ts
@@ -0,0 +1,53 @@
+import {AutoRegistry} from '../class/registry';
+import {Plugin} from '../class/plugin';
+import {BaseConfig} from '../util/config';
+import {Message, User, Member} from 'eris';
+import {Blueprint} from '../class/client';
+
+export class PluginRegistry<T extends BaseConfig> extends AutoRegistry<
+  Plugin<T>
+> {
+  /**
+   * Registers a new plugin
+   * @param value The plugin class to register
+   */
+  register(value: Plugin<T>): void {
+    const pluginData = {key: value.meta.name, value};
+    this.executeHook({message: 'Register Plugin', data: pluginData});
+    this.items.push(pluginData);
+  }
+
+  /**
+   * Removes an existing plugin
+   * @param key The name of the plugin
+   */
+  unregister(key: string): void {
+    const vk = this.items.findIndex(v => v.key === key);
+    if (vk > 0) {
+      this.items.splice(vk, 1);
+      this.executeHook({message: 'Unregister Plugin', data: {index: vk}});
+    }
+  }
+
+  /**
+   * Executes a command if the user has permissions to
+   * @param cmd The name of the command
+   * @param msg The message context
+   * @param user The user to check groups of
+   * @param ref The blueprint instance
+   */
+  execute(
+    cmd: string,
+    msg: Message,
+    user: User | Member,
+    args: Array<string>,
+    ref: Blueprint<T>
+  ) {
+    for (const {value} of this.items) {
+      if (value.has(cmd)) {
+        value.execute(cmd, msg, user, args, ref);
+        break;
+      }
+    }
+  }
+}
