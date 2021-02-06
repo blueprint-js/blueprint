ae5c0d2b8a460444bc8adb5119dae96785d2a3f1
Hanna <xpyxel@gmail.com>
2021-02-06 18:43:02 +0000

    Export the plugin stuff

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export {Blueprint, Extension, Internals, Registries} from './lib/class/client';
 export {Command, CommandMeta, Guard} from './lib/class/command';
 export {PermissionString} from './lib/util/permissions';
 export {Group, Override} from './lib/registry/groups';
+export {Plugin, PluginMeta} from './lib/class/plugin';
 export {ClientEvents} from './lib/util/types';
 export {Message, User, Member} from 'eris';
 export * as TypeORM from 'typeorm';
