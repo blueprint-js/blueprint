166b745498573f728567df0e3edcaa9715b25c6e
Jonathan <54381371+yaboyaxis@users.noreply.github.com>
2021-04-12 20:34:14 -0400

    fix(types): Moved plugin types to correct interface

--- src/lib/plugins/pluginTypes.ts
+++ src/lib/plugins/pluginTypes.ts
@@ -1,10 +1,14 @@
 export interface BasePluginData<T> {
     name: string;
     info?: any;
-
-    commands?: string[];
-    events?: string[];
 }
 
-export interface GuildPluginData<T> extends BasePluginData<T> {}
-export interface GlobalPluginData<T> extends BasePluginData<T> {}
+export interface GuildPluginData<T> extends BasePluginData<T> {
+    commands?: any; // TODO: Type
+    events?: any; // TODO: Type
+}
+
+export interface GlobalPluginData<T> extends BasePluginData<T> {
+    commands?: any; // TODO: Type
+    events?: any; // TODO: Type
+}
